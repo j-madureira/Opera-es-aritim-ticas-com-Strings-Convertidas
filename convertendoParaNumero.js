01164a78ea997b9d33e4aff7aa2a0490d636684f
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-11 16:23:34 -0300

    Add files via upload

---
+++ convertendoParaNumero.js
@@ -0,0 +1,31 @@
+/* 
+   Criar uma variável com o valor 4 do tipo String, Criar outra variável com
+   o valor 7 também do tipo String, criar mais uma variável, nessa variável
+   converter as Strings para números e então fazer a minha operação aritimética
+
+   Criar uma variável com o valor 3 do tipo String, Criar outra variável com
+   o valor 14 também do tipo String, criar mais uma variável, nessa variável
+   converter as Strings para números e então fazer a minha operação aritimética
+
+   Criar uma variável com o valor 14 do tipo String, Criar outra variável com
+   o valor 2 também do tipo String, criar mais uma variável, nessa variável
+   converter as Strings para números e então fazer a minha operação aritimética
+*/
+
+//Adição convertendo Strings para números
+var a = "4";
+var b = "7"
+var c = parseFloat(a) + parseFloat(b);
+console.log(c)
+
+//Subtração convertendo Strings para números
+var d = "3";
+var e = "14";
+var f = parseFloat(d) - parseFloat(e);
+console.log(f)
+
+//Divisão convertendo Strings para números
+var g = "14";
+var h = "2"
+var i = parseFloat(g) / parseFloat(h)
+console.log(i)
